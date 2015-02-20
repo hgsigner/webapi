d200828710cfe84bfd2f4726fa52903fd3ee44df
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-02-20 00:51:47 -0200

    implement basic user creation

--- users.go
+++ users.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/Schema"
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
-func uCollection(session *mgo.Session) *mgo.Collection {
-	return session.DB(AuthDatabase).C("users")
+var (
+	uRenderer *render.Render
+)
+
+func init() {
+	uRenderer = render.New()
 }
 
 type User struct {
@@ -21,6 +25,14 @@ type User struct {
 	FirstName string        `bson:"fisrt_name" json:"first_name" schema:"first_name"`
 	LastName  string        `bson:"last_name" json:"last_name" schema:"last_name"`
 	Email     string        `bson:"email" json:"email" schema:"email"`
+}
+
+type UserResults struct {
+	Users []User `json:"users"`
+}
+
+func uCollection(session *mgo.Session) *mgo.Collection {
+	return session.DB(AuthDatabase).C("users")
 }
 
 func wellcomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,17 +64,13 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-
 	newUser := User{}
-	err = collection.Find(bson.M{"email": u.Email}).One(&newUser)
+	err = collection.Find(bson.M{"_id": u.ID}).One(&newUser)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data, _ := json.Marshal(newUser)
-	w.Write(data)
+	uRenderer.JSON(w, http.StatusCreated, UserResults{[]User{newUser}})
 
 }
 
